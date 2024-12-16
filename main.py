2edd6928b47d772ee8ef6af0e7d2315739fcf48c
Raph <55244254+PhyrTheWall@users.noreply.github.com>
2024-12-16 12:34:42 -0500

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+"""Jeu Quixo
+
+Ce programme permet de joueur au jeu Quixo.
+"""
+
 from api import initialiser_partie, jouer_un_coup
 from quixo import Quixo, interpréter_la_commande
 
@@ -9,13 +14,13 @@ if __name__ == "__main__":
     args = interpréter_la_commande()
     id_partie, joueurs, plateau = initialiser_partie(args.idul, SECRET)
     while True:
-        # Créer un instance Quixo
+        # Créer une instance de Quixo
         quixo = Quixo(joueurs, plateau)
         # Afficher la partie
         print(quixo)
         # Demander au joueur de choisir son prochain coup
         origine, direction = quixo.choisir_un_coup()
-        # Envoyer le coup au serveur
+        # Envoyez le coup au serveur
         id_partie, joueurs, plateau = jouer_un_coup(
             id_partie,
             origine,
@@ -23,5 +28,6 @@ if __name__ == "__main__":
             args.idul,
             SECRET,
         )
+
     
     
